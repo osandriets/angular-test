800fb62c8f3d34f52894ae387262d390992955cd
osandriets@gmail.com <gjx2890-12345>
2018-08-05 19:10:18 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabase} from 'angularfire2/database';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  cuisine: Observable<any[]>;
+
+  itemValue = '';
+  items: Observable<any[]> = new Observable<any[]>();
+
+  constructor(public db: AngularFireDatabase) {
+    this.items = db.list('/data').valueChanges();
+    // db.list('/data').valueChanges().subscribe(x => {
+    //   this.items = x;
+    //   console.log('#', x);
+    // });
+  }
+
+  // onSubmit() {
+  //   this.db.list('/cuisine').push({ content: this.itemValue });
+  //   this.itemValue = '';
+  // }
+
+  // constructor(public db: AngularFireDatabase) {
+  //   console.log(db);
+  //
+  //   this.cuisine = db.list('/cuisine').valueChanges();
+  //
+  //     console.log(this.cuisine);
+  //   // db.list('/cuisine').subscribe(x => {
+  //   //   this.cuisine = x;
+  //   // });
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseCongige = {
+  apiKey: "AIzaSyCLCs6VbdU2y7nzKv6jyEEiQSzFKuEegHs",
+  authDomain: "testang-33df0.firebaseapp.com",
+  databaseURL: "https://testang-33df0.firebaseio.com",
+  projectId: "testang-33df0",
+  storageBucket: "testang-33df0.appspot.com",
+  messagingSenderId: "938737977259"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseCongige),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
